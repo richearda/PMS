0462f1f3fbfc3d53a538e2f2f6aa42f160cc7191
Riche Arda <richearda1996@gmail.com>
2023-03-01 23:15:58 +0800

    Modify method in AccountService
    Rename the method and param and change the param type

--- Purok Management System/UI/PMS-UI/src/app/services/account.service.ts
+++ Purok Management System/UI/PMS-UI/src/app/services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { RegisterUserDto } from '../dto/command/registeruser.dto';
 import { AppUser } from '../models/appuser.model';
 
 @Injectable({
@@ -13,8 +14,9 @@ export class AccountService {
 }
 
 //Register the user
-registerUser(userToRegister:AppUser){
-  return this.http.post(this.baseUrl + "Account/Register", userToRegister);
+registerPerson(personToRegister:RegisterUserDto){
+  console.log("Service: Person to Register " + JSON.stringify(personToRegister))
+  return this.http.post(this.baseUrl + "Account/Register", personToRegister);
 }
 
 
